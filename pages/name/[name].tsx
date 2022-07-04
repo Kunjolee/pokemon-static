216455968d3aac23e0826236460e47dbeb4e4a91
Josuelee <josuelee28@gmail.com>
2022-07-04 14:06:01 -0600

    ISR functionality added

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -29,7 +29,7 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
 
   return {
     paths,
-    fallback: false
+    fallback: "blocking"
   }
 }
 
@@ -37,11 +37,37 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   let { name } = params as { name: string}      
 
+  const pokemon = await getPokemonInfo(name);
+
+  if (!pokemon) {
+    return {
+      redirect:{
+        destination: "/",
+        permanent: false
+      }
+    }
+  }
+  
   return {
     props: {
-      pokemon: await getPokemonInfo(name)
-    }
+      pokemon
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 86400 //in seconds
   }
 }
 
 export default PokemonByNamePage 
+
+/* 
+  ISR: Incremental static regeneration   
+  Me va a regenerar mi aplicacion cada cierto tiempo, ya estando en produccion. 
+
+  puede actualizar la pagina o crear una nueva, sin necesidad de volver a construir la aplicacion. Es decir estando ya en produccion puede realizar todos esos procesos, de creacion o actualizacion de una pagina.
+
+  Esto sirve por ejemplo si mi aplicacion yo se que su informacion va a cambiar cada cierto tiempo. Estando ya en produccion las paginas son estaticas, pero al re construirlas. traera esa informacion que ha cambiado con el paso del tiempo
+
+  Esto se ejecutara cuando despues de el tiempo establecido, el cliente haga una request a la pagina que tuvo su regeneracion. ahi aplicara el cambio que despues a todos los demas clientes que hagan una request, ya tendran el cambio
+*/
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,11 +2,17 @@ import { pokeApi } from "../api"
 import { Pokemon } from "../interfaces"
 
 export const getPokemonInfo = async ( nameOrId: string ) => {
-  const { data } = await pokeApi.get<Pokemon>(`pokemon/${nameOrId}`);
 
-  return  {
-    name: data.name,
-    sprites: data.sprites,
-    id: data.id
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`pokemon/${nameOrId}`);
+  
+    return  {
+      name: data.name,
+      sprites: data.sprites,
+      id: data.id
+    }
+    
+  } catch (error) {
+    return null;          
   }
 }
